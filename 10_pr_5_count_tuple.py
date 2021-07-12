107c92844bcbb82901d2c6f617ddc16bf5ea6646
UKA-SHA <72197034+UKA-SHA@users.noreply.github.com>
2021-07-11 23:30:47 -0700

    Update 10_pr_5_count_tuple.py

--- 10_pr_5_count_tuple.py
+++ 10_pr_5_count_tuple.py
@@ -1,3 +1,4 @@
-# write a program to count the numbers of zeros in the following tuple:
+#write a program to count the numbers of zeros in the following tuple:
+
 tup1 = (7,0,8,0,0,9)
 print(tup1.count(0))
